667e1074b2842864985229885fa3fa07fee2a721
Martin Lindström <martin.lindstrom@litsec.se>
2020-10-08 17:01:14 +0200

    Fixed warning

--- src/main/java/se/idsec/signservice/integration/document/pdf/PdfSignatureImageTemplate.java
+++ src/main/java/se/idsec/signservice/integration/document/pdf/PdfSignatureImageTemplate.java
@@ -25,9 +25,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.Singular;
 import lombok.ToString;
 import se.idsec.signservice.integration.core.Extensible;
