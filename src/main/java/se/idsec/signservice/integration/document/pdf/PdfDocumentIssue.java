54f3212d13ad0b52aaab02b6cabe93ff3464b718
Stefan Santesson <stefan@aaa-sec.com>
2024-08-29 15:05:03 +0200

    Documentation for PdfDocumentIssue enum.

--- src/main/java/se/idsec/signservice/integration/document/pdf/PdfDocumentIssue.java
+++ src/main/java/se/idsec/signservice/integration/document/pdf/PdfDocumentIssue.java
@@ -7,10 +7,16 @@ package se.idsec.signservice.integration.document.pdf;
  * @author Stefan Santesson (stefan@idsec.se)
  */
 public enum PdfDocumentIssue {
-  /**  */
+  /**
+   * The document is unsigned but has an AcroForm that may contain user input form fields.
+   * This AcroForm must be rendered and the AcroForm must be deleted before signing
+   */
   ACROFORM_IN_UNSIGNED_PDF,
 
-  /**  */
+  /**
+   * The document has an encryption dictionary.
+   * No changes to the document can be saved before removing all security policies, which means deleting the encryption dictionary
+   */
   ENCRYPTION_DICTIONARY
 
 }
